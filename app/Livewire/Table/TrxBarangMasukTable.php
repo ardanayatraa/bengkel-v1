4da6e13430c1e2b28651707847b824a9e0828e76
I Made Ardana Yatra <141602961+ardanayatraa@users.noreply.github.com>
2025-07-01 13:57:05 +0800

    trx barang masuk filter

--- app/Livewire/Table/TrxBarangMasukTable.php
+++ app/Livewire/Table/TrxBarangMasukTable.php
@@ -3,8 +3,11 @@
 namespace App\Livewire\Table;
 
 use App\Models\TrxBarangMasuk;
+use App\Models\Barang;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
+use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
+use Rappasoft\LaravelLivewireTables\Views\Filters\DateFilter;
 
 class TrxBarangMasukTable extends DataTableComponent
 {
@@ -15,16 +18,50 @@ class TrxBarangMasukTable extends DataTableComponent
         $this->setPrimaryKey('id_trx_barang_masuk');
     }
 
+    /**
+     * Definisi filters: filter per barang & rentang tanggal.
+     */
+    public function filters(): array
+    {
+        return [
+            // Dropdown pilih Barang
+            SelectFilter::make('Barang')
+                ->options(
+                    // ambil daftar barang: [id => nama]
+                    Barang::orderBy('nama_barang')
+                          ->pluck('nama_barang', 'id_barang')
+                          ->toArray()
+                )
+                ->filter(function($query, $value) {
+                    if ($value) {
+                        $query->where('id_barang', $value);
+                    }
+                }),
+
+            // Tanggal Mulai (>=)
+            DateFilter::make('Dari')
+                ->filter(function($query, $value) {
+                    $query->whereDate('tanggal_masuk', '>=', $value);
+                }),
+
+            // Tanggal Akhir (<=)
+            DateFilter::make('Sampai')
+                ->filter(function($query, $value) {
+                    $query->whereDate('tanggal_masuk', '<=', $value);
+                }),
+        ];
+    }
+
     public function columns(): array
     {
         return [
             Column::make("Id trx barang masuk", "id_trx_barang_masuk")
                 ->sortable()
                 ->format(
-                    fn($value, $row, Column $column) =>
+                    fn($value) =>
                         '<a href="'.route('trx-barang-masuk.show', $value).'" class="text-blue-600 hover:underline">'.$value.'</a>'
                 )
-                ->html(),  // jangan lupa ini biar HTML-nya di-render
+                ->html(),
 
             Column::make("Barang", "barang.nama_barang")
                 ->sortable(),
