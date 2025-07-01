2cb67559fe8aacbf827801a9e65d984f0af68e06
I Made Ardana Yatra <141602961+ardanayatraa@users.noreply.github.com>
2025-07-01 09:49:02 +0800

    show trx

--- resources/views/transaksi/show.blade.php
+++ resources/views/transaksi/show.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-    <div class="mx-auto max-w-4xl sm:px-6 lg:px-8 py-6">
+    <div class="mx-auto w-full sm:px-6 lg:px-8 py-6">
         {{-- Card Container --}}
         <div class="bg-white shadow-md rounded-lg overflow-hidden">
             {{-- Header --}}
